8c563ccafad23997dd3add5cc0f5aad69b349ec0
DERRICK OBENG <derickobeng023@gmail.com>
2025-07-03 21:16:24 +0000

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,6 +1,8 @@
+
+
 -- USERS table
 CREATE INDEX idx_users_email ON users(email);
-CREATE INDEX idx_users_id ON users(id);  -- optional; often already indexed if primary key
+CREATE INDEX idx_users_id ON users(id);  -- optional; often indexed as PK
 
 -- BOOKINGS table
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
@@ -10,14 +12,24 @@ CREATE INDEX idx_bookings_start_date ON bookings(start_date);
 -- PROPERTIES table
 CREATE INDEX idx_properties_location ON properties(location);
 CREATE INDEX idx_properties_price ON properties(price);
-CREATE INDEX idx_properties_id ON properties(id);  -- optional; often already indexed
+CREATE INDEX idx_properties_id ON properties(id);  -- optional; often indexed as PK
 
+-- Query performance before and after using EXPLAIN ANALYZE
 
-
-
-EXPLAIN
-SELECT 
-  u.name, COUNT(b.id)
+-- BEFORE Indexing
+EXPLAIN ANALYZE
+SELECT u.name, COUNT(b.id)
 FROM users u
 JOIN bookings b ON u.id = b.user_id
 GROUP BY u.name;
+
+-- AFTER Indexing
+EXPLAIN ANALYZE
+SELECT u.name, COUNT(b.id)
+FROM users u
+JOIN bookings b ON u.id = b.user_id
+GROUP BY u.name;
+
+
+
+
