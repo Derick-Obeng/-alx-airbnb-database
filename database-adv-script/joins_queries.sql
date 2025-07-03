1aba9e11fcab73a607ff696f6ba4adc52f3de1a8
DERRICK OBENG <derickobeng023@gmail.com>
2025-07-03 20:24:56 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -18,7 +18,9 @@ SELECT
   reviews.rating,
   reviews.comment
 FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
+
 
 
 
