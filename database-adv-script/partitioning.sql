57b210484124952b63d0499d11475f8642a03a1e
DERRICK OBENG <derickobeng023@gmail.com>
2025-07-05 12:08:18 +0000

    Create partitioning.sql

---
+++ database-adv-script/partitioning.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS bookings CASCADE;
+
+CREATE TABLE bookings (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  property_id INT,
+  start_date DATE,
+  end_date DATE,
+  status VARCHAR(50)
+) PARTITION BY RANGE (start_date);
+
+
+
+CREATE TABLE bookings_2023 PARTITION OF bookings
+  FOR VALUES FROM ('2023-01-01') TO ('2024-01-01');
+
+CREATE TABLE bookings_2024 PARTITION OF bookings
+  FOR VALUES FROM ('2024-01-01') TO ('2025-01-01');
+
+CREATE TABLE bookings_2025 PARTITION OF bookings
+  FOR VALUES FROM ('2025-01-01') TO ('2026-01-01');
+
+
+EXPLAIN ANALYZE
+SELECT * FROM bookings
+WHERE start_date BETWEEN '2024-06-01' AND '2024-06-30';
